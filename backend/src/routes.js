53c853f109f3904f2b72544e9c093be2287277db
Diego Mais <diegomais@live.com>
2019-10-01 17:55:30 +0200

    Create routes file
    - This file manages the routes in the application.
    - This architecture keeps routes and server files clean.
    
    On branch master
    Changes to be committed:
    	new file:   backend/src/routes.js
    	modified:   backend/src/server.js

---
+++ backend/src/routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => res.send('Hello World!'));
+
+module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
