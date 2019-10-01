849976e75d81a0806453d6f3e3370573e915e5e2
Diego Mais <diegomais@live.com>
2019-10-01 21:46:30 +0200

    Add show dashboard feature
    - This feature provides user to show the spots they have registered.
    
    On branch master
    Changes to be committed:
    	new file:   backend/src/controllers/DashboardController.js
    	modified:   backend/src/routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -12,5 +13,7 @@ routes.post('/sessions', SessionController.store);
 
 routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
 
 module.exports = routes;
