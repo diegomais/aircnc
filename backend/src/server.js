4a8f4cfab86788bcc4ca8b78f16fa4affe0c45e6
Diego Mais <diegomais@live.com>
2019-10-01 20:20:16 +0200

    Configure and connect database
    - MongoDB Atlas is MongoDB's managed MongoDB as a Service.
    - Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment.
    
    On branch master
    Changes to be committed:
    	modified:   backend/package.json
    	modified:   backend/src/server.js
    	modified:   backend/yarn.lock

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb+srv://aircnc:Ewky4oDdrrWp2mVC@aircnc-d8eag.mongodb.net/aircnc?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 app.use(express.json());
 app.use(routes);
