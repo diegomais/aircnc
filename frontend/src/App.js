077eba18591454fb2c11a95ae2aecf58d77312c3
Diego Mais <diegomais@live.com>
2019-10-02 16:42:45 +0200

    Add sign in page UI
    - User should sign in to app.
    
    On branch master
    Changes to be committed:
    	modified:   frontend/src/App.css
    	modified:   frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
-  return <h1>AirCnC</h1>;
+  return (
+    <div className="container">
+      <img src={logo} alt="AirCnC" />
+
+      <div className="content">
+        <p>
+          Offer <strong>spots</strong> for software developers and find
+          <strong> talents</strong> for your company.
+        </p>
+
+        <form>
+          <label htmlFor="email">
+            E-mail
+            <input
+              id="email"
+              type="email"
+              placeholder="Enter your e-mail address"
+            />
+          </label>
+          <button type="submit" className="btn">
+            Sign in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
