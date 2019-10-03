28516500f8cc129568e1e2faeac903db663a6285
Diego Mais <diegomais@live.com>
2019-10-03 15:21:36 +0200

    Add dashboard page
    - User should have a page to manage their spots.
    
    On branch master
    Changes to be committed:
    	modified:   frontend/src/pages/Dashboard/index.js
    	new file:   frontend/src/pages/Dashboard/styles.css

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,7 +1,39 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+import './styles.css';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    async function loadSpots() {
+      const user_id = localStorage.getItem('aircnc:user');
+      const response = await api.get('/dashboard', { headers: { user_id } });
+
+      setSpots(response.data);
+    }
+
+    loadSpots();
+  }, []);
+
+  return (
+    <>
+      <ul className="spot-list">
+        {spots.map(spot => (
+          <li key={spot._id}>
+            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
+            <strong>{spot.company}</strong>
+            <span>{spot.price ? `R$${spot.price}/dia` : 'Gratuito'}</span>
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/add">
+        <button type="button" className="btn">
+          Add new spot
+        </button>
+      </Link>
+    </>
+  );
 }
