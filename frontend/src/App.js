cc92effa0ec06a4e1c4153f1c47d79376f16922a
Diego Mais <diegomais@live.com>
2019-10-03 14:19:25 +0200

    Add submit sign in feature
    - User should sign in to app.
    - Add axios to make requests on server.
    - Store user ID on browser local storage
    
    On branch master
    Changes to be committed:
    	modified:   frontend/package.json
    	modified:   frontend/src/App.js
    	new file:   frontend/src/services/api.js
    	modified:   frontend/yarn.lock

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post('/sessions', { email });
+
+    localStorage.setItem('aircnc:user', response.data._id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC" />
@@ -14,13 +25,15 @@ function App() {
           <strong> talents</strong> for your company.
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">
             E-mail
             <input
               id="email"
               type="email"
               placeholder="Enter your e-mail address"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
             />
           </label>
           <button type="submit" className="btn">
