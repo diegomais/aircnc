9c17e1e476b8415c3e0e5496e3a49d9d6b1d8c81
Diego Mais <diegomais@live.com>
2019-10-03 14:15:13 +0200

    Add Cross-Origin Resource Sharing (CORS)
    - CORS is needed because it allows who can access server assets.
    
    On branch master
    Changes to be committed:
    	modified:   backend/package.json
    	modified:   backend/src/server.js
    	modified:   backend/yarn.lock

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
