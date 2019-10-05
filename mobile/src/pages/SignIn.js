c2dcdc67b6e21f680d7be39d70a85ec65abba418
Diego Mais <diegomais@live.com>
2019-10-05 10:23:29 +0200

    Add SignIn page
    - User must sign in to mobile app.
    
    On branch master
    Changes to be committed:
    	modified:   mobile/package.json
    	modified:   mobile/src/pages/SignIn.js
    	new file:   mobile/src/services/api.js
    	modified:   mobile/yarn.lock

--- mobile/src/pages/SignIn.js
+++ mobile/src/pages/SignIn.js
@@ -1,10 +1,117 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  AsyncStorage,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
 
-export default function SignIn() {
+import api from '../services/api';
+
+import logo from '../../assets/logo.png';
+
+export default function SignIn({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('aircnc:user').then(user => {
+      if (user) {
+        navigation.navigate('List');
+      }
+    });
+  }, []);
+
+  async function handleSubmit() {
+    const response = await api.post('/sessions', { email });
+
+    await AsyncStorage.setItem('aircnc:user', response.data._id);
+    await AsyncStorage.setItem('aircnc:techs', techs);
+
+    navigation.navigate('List');
+  }
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <KeyboardAvoidingView
+      enabled={Platform.OS === 'ios'}
+      behavior="padding"
+      style={styles.container}
+    >
+      <Image source={logo} />
+
+      <View style={styles.form}>
+        <Text style={styles.label}>Seu e-mail</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your e-mail"
+          placeholderTextColor="#999"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={email}
+          onChangeText={setEmail}
+        />
+
+        <Text style={styles.label}>Technologies</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Interested techs"
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+          value={techs}
+          onChangeText={setTechs}
+        />
+
+        <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+          <Text style={styles.buttonText}>Search spots</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  form: {
+    alignSelf: 'stretch',
+    paddingHorizontal: 30,
+    marginTop: 30
+  },
+  label: {
+    fontWeight: 'bold',
+    color: '#444',
+    marginBottom: 8
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    paddingHorizontal: 16,
+    fontSize: 16,
+    color: '#444',
+    height: 44,
+    marginBottom: 20,
+    borderRadius: 4
+  },
+  button: {
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16
+  }
+});
