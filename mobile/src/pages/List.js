162ead808c544e10143cc49d0a1a30396281f653
Diego Mais <diegomais@live.com>
2019-10-05 11:22:54 +0200

    Add list page
    - User must list spots by interested tech.
    
    On branch master
    Changes to be committed:
    	new file:   mobile/src/components/SpotList.js
    	modified:   mobile/src/pages/List.js

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  AsyncStorage,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet
+} from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../../assets/logo.png';
 
 export default function List() {
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('aircnc:techs').then(storedTechs => {
+      setTechs(storedTechs.split(',').map(item => item.trim()));
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>List</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 10
+  }
+});
