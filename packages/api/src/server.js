c0df00e9040e3030632c9d344a27b5bc1618dbf2
Diego Mais <diegomais.dev@gmail.com>
2020-09-08 22:52:39 -0300

    🐛 Add Sentry error tracking

--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const path = require('path');
 const socketio = require('socket.io');
 const http = require('http');
+const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
 const routes = require('./routes');
 const PORT = process.env.PORT || 3333;
 const app = express();
@@ -14,6 +16,15 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -21,7 +32,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 
 const connectedUsers = {};
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   // Get user_id sent by client
   const { user_id } = socket.handshake.query;
 
@@ -43,7 +54,17 @@ app.use((req, res, next) => {
 
 app.use(cors());
 app.use(express.json());
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+app.get('/debug-sentry', (req, res) => {
+  throw new Error('My first Sentry error!');
+});
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
+app.use(Sentry.Handlers.errorHandler());
+app.use((err, req, res, next) => {
+  res.statusCode = 500;
+  res.end(res.sentry + '\n');
+});
 
 server.listen(PORT);
