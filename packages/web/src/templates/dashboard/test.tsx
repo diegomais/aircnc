70014577f9dca5748c87ad4b484cfa69e252ca32
Diego Mais <diegomais.dev@gmail.com>
2021-04-18 19:22:38 -0300

    ✅ (web): add Jest and Testing Library

---
+++ packages/web/src/templates/dashboard/test.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { api } from '../../services/api'
+import { DashboardTemplate } from '.'
+
+describe('<DashboardTemplate />', () => {
+  jest.mock('socket.io-client')
+  jest.mock('../../services/api')
+  const mockedApi = api as jest.Mocked<typeof api>
+  mockedApi.get = jest.fn().mockResolvedValue({
+    data: [{ _id: 'foo', company: 'bar', thumbnail_url: 'image.png' }],
+  })
+
+  it('matches snapshot', async () => {
+    const { asFragment } = render(<DashboardTemplate />)
+    await screen.findByText(/bar/i)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should show spot list', async () => {
+    render(<DashboardTemplate />)
+    expect(await screen.findByText(/bar/i)).toBeInTheDocument()
+  })
+
+  it('should show a button to add a new spot', async () => {
+    render(<DashboardTemplate />)
+    await screen.findByText(/bar/i)
+    expect(screen.getByText(/add new spot/i)).toBeInTheDocument()
+  })
+})
